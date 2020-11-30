500a66c7b41efe51968f712c2de5f8f1b824e740
DongJin Shin <dongjin.shin.00@gmail.com>
2020-11-30 10:20:23 +0900

    feat: add image masking

--- app.py
+++ app.py
@@ -3,13 +3,29 @@ from werkzeug.utils import secure_filename
 import redis
 import os
 from pydicom import dcmread
-from pydicom.pixel_data_handlers import apply_windowing, apply_rescale
 from base64 import b64encode
 from PIL import Image
 import io
 import numpy as np
 from waitress import serve
 import shutil
+import zipfile
+import colorsys
+import random
+import glob
+
+
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    To get visually distinct colors, generate them in HSV space then
+    convert to RGB.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
 
 
 def create_app():
@@ -62,9 +78,29 @@ def launch():
 
         prepare_storage()
         f.save(fpath)
+        with zipfile.ZipFile(fpath) as zf:
+            extract_path = os.path.join(app.config['UPLOAD_FOLDER'], 'extract')
+            os.mkdir(extract_path)
+            zf.extractall(extract_path)
 
-        # TODO: model prediction
-        image = b64encode(dcm_to_jpg(fpath)).decode()
+        # TODO
+        # ==================================================================
+        ct_array = dcm_to_npy(extract_path)     # 3D CT array
+        segmentation = np.zeros(ct_array.shape)    # model prediction
+        
+        # projection
+        sample_idx = np.argmax(np.sum(ct_array, axis=(0, 1)))
+        original_image = ct_array[..., sample_idx]
+        segmentation_image = segmentation[..., sample_idx]
+
+        color = random_colors(1)[0]
+        merged_image = apply_mask(to_rgb(original_image), segmentation_image, color)
+        # ==================================================================
+        
+        im = Image.fromarray(merged_image.astype(np.uint8))
+        img_bytes = io.BytesIO()
+        im.save(img_bytes, format='PNG')
+        image = b64encode(img_bytes.getvalue()).decode()
 
         cleanup_storage()
         return {'image': image}
@@ -74,23 +110,40 @@ def launch():
         r.delete('lock')    # unlock
 
 
-def dcm_to_jpg(dcm_path):
-    ds = dcmread(dcm_path)
+def to_rgb(gray_image):
+    return np.stack([gray_image.copy() for _ in range(3)], axis=2)
 
-    wc = ds.get('WindowCenter', 127.5)
-    ww = ds.get('WindowWidth', 255)
-    intercept = ds.get('RescaleIntercept', 0)
-    slope = ds.get('RescaleSlope', 1)
 
-    arr = ds.pixel_array
-    arr = arr * slope + intercept
-    arr = (arr - wc) * 255. / ww + 127.5;
-    im = Image.fromarray(arr.astype(np.uint8))
-    im = im.resize((128, 128))
-    img_bytes = io.BytesIO()
-    im.save(img_bytes, format='JPEG')
-    return img_bytes.getvalue()
+def apply_mask(image, mask, color, alpha=0.5):
+    for c in range(3):
+        image[:, :, c] = np.where(mask == 1,
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+    return image
+
+
+def dcm_to_npy(dcm_dir):
+    dcm_list = sorted(glob.glob(dcm_dir + '/**/*.dcm'))
+    ct_array = []
+    for dcm_file_path in dcm_list:
+        ds = dcmread(dcm_file_path)
+        wc = ds.get('WindowCenter', 127.5)
+        ww = ds.get('WindowWidth', 255)
+        intercept = ds.get('RescaleIntercept', 0)
+        slope = ds.get('RescaleSlope', 1)
+
+        arr = np.float32(ds.pixel_array)
+        arr = arr * slope + intercept
+        arr = (arr - wc) / ww + 0.5;
+
+        ct_array.append(arr)
+
+    ct_array = np.stack(ct_array, axis=2)
+    ct_array = np.clip(ct_array, 0, 1)
+    ct_array = np.uint8(ct_array * 255)
+    return ct_array
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=7889)
--- run_dev.sh
+++ run_dev.sh
@@ -1,1 +1,1 @@
-FLASK_ENV=development FLASK_APP=app.py flask run --host 0.0.0.0
+FLASK_ENV=development FLASK_APP=app.py flask run --host 0.0.0.0 --port 7889
